c86d0aebcbac7bb7f5b793d747fbc72ed8655a77
kafey <dafeb.rules@gmail.com>
2022-07-12 06:12:09 +0700

    filter nc report

--- app/audit/views.py
+++ app/audit/views.py
@@ -47,15 +47,17 @@ class AuditViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         """Retrieve personels for authenticated user."""
         user = self.request.user
-        # if user.is_staff:
-        #     return self.queryset.filter(user=self.request.user).order_by('-id')
+        
         
         sub_area = self.request.query_params.get('sub-area')
         queryset = self.queryset
         if sub_area:
             sub_area_id = sub_area
             queryset = queryset.filter(sub_area=sub_area_id)
-            
+        
+        if user.is_staff:
+            return queryset.filter(user=self.request.user).order_by('-id').distinct()
+
         return queryset.order_by('-id').distinct()
 
     def get_serializer_class(self):
