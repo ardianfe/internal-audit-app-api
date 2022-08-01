fa5dcad57b329bf7248eccac6a823fbc063bfef6
kafey <dafeb.rules@gmail.com>
2022-08-01 15:02:12 +0700

    is_superuser

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -15,7 +15,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = get_user_model()
-        fields = ['id', 'email', 'password', 'name', 'is_staff']
+        fields = ['id', 'email', 'password', 'name', 'is_staff', 'is_superuser']
         extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
 
     def create(self, validated_data):
